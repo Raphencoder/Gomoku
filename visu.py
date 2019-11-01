98563414e1e3aa4b1b83b95e2a9f6f8af4402a2d
Raphael Krief <rkrief@student.42.fr>
2019-11-01 14:35:30 +0100

    fix map to 19 + add possibily to choose size of map + fix bug of unplayed player when starting game

--- visu.py
+++ visu.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import sys
 from menu import game_intro
-from variables import inters
+# from variables import inters
 import time
 
 
@@ -21,36 +21,50 @@ def in_inter(pos, inters):
     for tup in inters:
         if (pos[0] >= tup[0] and pos[0] <= tup[2])\
             and (pos[1] >= tup[1] and pos[1] <= tup[3]):
-            return tup[0] + 10, tup[1] + 10
+            return (tup[0] + 10), (tup[1] + 10)
     return 0, 0
 
-"""
-def get_inter():
-    y = 30
+
+def get_inter(nb_square, gomoku):
     inters = []
-    for i in range(15):
+    size = 700 + nb_square
+    while size % nb_square != 0:
+        size += 1
+    size_square = (size / nb_square)
+    y = 30
+    for i in range(nb_square + 1):
         x = 30
-        for l in range(15):
-            square_inter_start = (x - 30, y - 30)
-            square_inter_end = (x + 30, y + 30)
+        for l in range(nb_square + 1):
+            square_inter_start = (x - 10, y - 10)
+            square_inter_end = (x + 10, y + 10)
             square_inter = square_inter_start + square_inter_end
+            # allow to see the click point
+            # pygame.draw.rect(gomoku.window, (0, 0, 0), (x - 10, y - 10, 20, 20), 2) # largeur
             inters.append(square_inter)
-            x += 52
-        y += 52
+            x += size_square 
+        y += size_square 
     return inters
-"""
+
+def get_coordinate(nb_square):
+    cord = []
+    for x in range(nb_square + 1):
+        for y in range(nb_square + 1):
+            cord.append([x, y, -1])
+    return cord
+
+
 
 class Gomoku():
 
-    def __init__(self):
+    def __init__(self, ai_mode):
         pygame.init()
         self.window = pygame.display.set_mode((800, 800))
         pygame.display.set_caption("gomoku")
-        self.img_player_one = pygame.image.load("joueur-blanc.png").convert_alpha()
-        self.img_player_two = pygame.image.load("joueur-noir.png").convert_alpha()
-        self.inters = inters
+        self.img_player_one = pygame.image.load("joueur-blanc-petit.png").convert_alpha()
+        self.img_player_two = pygame.image.load("joueur-noir-petit.png").convert_alpha()
         self.pos_player = []
         self.coordinate = []
+        self.ai_mode = ai_mode
         self.current_player = 1
 
     def can_place(self, x, y):
@@ -68,11 +82,11 @@ class Gomoku():
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP and (not self.ai_mode or self.current_player == 1):
                 pos = pygame.mouse.get_pos()
-                x_player, y_player = in_inter(pos, inters)
+                x_player, y_player = in_inter(pos, self.inters)
                 if x_player != 0 and y_player != 0:
                     if self.can_place(x_player, y_player):
                         self.pos_player.append(((x_player,) + (y_player,) + (self.current_player,)))
-                        pos = [int((x_player - 10)/52), int((y_player - 10)/52), -1]
+                        pos = [int((x_player - 10)/38), int((y_player - 10)/38), -1]
                         i = self.coordinate.index(pos)
                         self.coordinate[i][2] = self.current_player
                         print (self.coordinate[i])
@@ -83,38 +97,43 @@ class Gomoku():
                 quit()
 
 
-    def fill_background(self):
+    def fill_background(self, nb_square):
         self.window.fill(yellow)
-        pygame.draw.rect(self.window, (0, 0, 0), (30, 30, 730, 730), 2)
-        L = 700
-        nb_square = range(15)
-        size_square = int(L / max(nb_square) + 2)
-        for i in nb_square:
-            cube_pos = size_square * i
-            pygame.draw.line(self.window, (0, 0, 0), (30, 30 + cube_pos), (730 + 30, 30 + cube_pos), 1) # largeur
-            pygame.draw.line(self.window, (0, 0, 0), (30 + cube_pos, 30), (30 + cube_pos, 730 + 30), 1) # longueur
+        size = 700 + nb_square
+        while size % nb_square != 0:
+            size += 1
+        pygame.draw.rect(self.window, (0, 0, 0), (30, 30, size, size), 2)
+        size_square = size / nb_square
+        for i in range(nb_square):
+            cube_pos = (size_square * i)
+            pygame.draw.line(self.window, (0, 0, 0), (30, 30 + cube_pos), (size + 30, 30 + cube_pos), 1) # largeur
+            pygame.draw.line(self.window, (0, 0, 0), (30 + cube_pos, 30), (30 + cube_pos, size + 30), 1) # longueur
 
     def display_player(self):
         for elem in self.pos_player:
             if elem[2] == 1:
-                self.window.blit(self.img_player_one, (elem[0], elem[1]))
+                self.window.blit(self.img_player_one, (elem[0] - 12, elem[1] - 12))
             else:
-                self.window.blit(self.img_player_two, (elem[0], elem[1]))
+                self.window.blit(self.img_player_two, (elem[0] - 12, elem[1] - 12))
         pygame.display.flip()
 
 
+
 def start_game():
-    gomoku = Gomoku()
-    gomoku.init = True
-    for x in range(15):
-        for y in range(15):
-            gomoku.coordinate.append([x, y, -1])
+    
+    pygame.init()
+    ai_mode = game_intro(pygame.display.set_mode((800, 800)))
+    pygame.quit()
+    nb_square = 19
+    gomoku = Gomoku(ai_mode)
+
+    gomoku.coordinate = get_coordinate(nb_square)
+    gomoku.inters = get_inter(nb_square, gomoku)
     while True:
-        if gomoku.init:
-            gomoku.ai_mode = game_intro(gomoku.window)
-            gomoku.init = False
         gomoku.check_event()
-        gomoku.fill_background()
         gomoku.display_player()
+        gomoku.fill_background(nb_square)
+
+
 
 start_game()
